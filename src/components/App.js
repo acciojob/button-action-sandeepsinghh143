ec45bb421ba8d08568fd8ef0141c19ce4e7804ca
sandeepsinghh143 <sandeepsinghh143@gmail.com>
2023-07-16 12:11:19 +0530

    submitting code-Sun Jul 16 2023 12:11:18 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,16 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+  let [text,setText]=useState("");
+  function showText(){
+    setText("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  }
 
   return (
     <div className="App" id="main">
       // Do not alter the main div
+      <p id="para">{text}</p>
+      <button id="click" onClick={showText}>Show Text</button>
     </div>
   );
 }
